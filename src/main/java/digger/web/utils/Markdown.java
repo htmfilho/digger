ac9487f4f8f812e887c41f8576ebb80286800c5d
Hildeberto Mendonca <hmendonca@univeris.com>
2019-09-16 17:17:59 -0400

    Added support for tables in the markdown content.

--- src/main/java/digger/web/utils/Markdown.java
+++ src/main/java/digger/web/utils/Markdown.java
@@ -1,19 +1,25 @@
 package digger.web.utils;
 
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class Markdown {
 
-    Parser parser;
-    HtmlRenderer renderer;
+    private Parser parser;
+    private HtmlRenderer renderer;
 
     public Markdown() {
-        parser = Parser.builder().build();
-        renderer = HtmlRenderer.builder().build();
+        List<Extension> extensions = Arrays.asList(TablesExtension.create());
+        parser = Parser.builder().extensions(extensions).build();
+        renderer = HtmlRenderer.builder().extensions(extensions).build();
     }
 
     public String toHtml(String markdown) {
