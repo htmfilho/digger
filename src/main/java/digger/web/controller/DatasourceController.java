8aa09e66d87f4b4acbdd6a06f3539b1ec0eea246
Hildeberto Mendonca <me@hildeberto.com>
2019-09-25 23:32:15 -0400

    #5 Implemented the documentation progress of tables.

--- src/main/java/digger/web/controller/DatasourceController.java
+++ src/main/java/digger/web/controller/DatasourceController.java
@@ -34,7 +34,11 @@ public class DatasourceController {
     public String openDatasource(Model model, @PathVariable Long datasourceId) {
         Datasource datasource = datasourceService.findById(datasourceId);
         model.addAttribute("datasource", datasource);
-        model.addAttribute("progress", (tableService.countDocumentedTables(datasource) / datasource.getTotalTables()) * 100);
+        if(datasource.getTotalTables() != null && datasource.getTotalTables() > 0) {
+            model.addAttribute("progress", Math.round(tableService.countDocumentedTables(datasource) / (datasource.getTotalTables() * 1.0) * 100));
+        } else {
+            model.addAttribute("progress", 0);
+        }
         return "datasource";
     }
 
