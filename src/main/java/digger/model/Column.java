6b7d401b96cd6ebd74301a85cb7b60c347d50dc2
Hildeberto Mendonca <me@hildeberto.com>
2020-03-14 18:10:32 -0400

    Changed the id from primitive to class Long

--- src/main/java/digger/model/Column.java
+++ src/main/java/digger/model/Column.java
@@ -49,11 +49,11 @@ public class Column implements Comparable<Column> {
         this.name = name;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/digger/model/Table.java
+++ src/main/java/digger/model/Table.java
@@ -40,11 +40,11 @@ public class Table implements Comparable<Table> {
         this.name = name;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
