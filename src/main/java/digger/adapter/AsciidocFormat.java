083aef3312ecebcd0d6080cc57cd5bbecb52c4af
Hildeberto Mendonca <me@hildeberto.com>
2020-03-29 14:44:39 -0400

    Fixed punctual Asciidoc formatting issues and replaced StringBuilder by String.

--- src/main/java/digger/adapter/AsciidocFormat.java
+++ src/main/java/digger/adapter/AsciidocFormat.java
@@ -13,60 +13,50 @@ public class AsciidocFormat implements DocumentationFormat {
 
     @Override
     public String applyFormatToDatasource(Datasource datasource) {
-        StringBuilder content = new StringBuilder();
-        content.append("= ");
-        content.append(datasource.getName());
-        content.append("\nv0.5.0, 2020-03-24\n\n");
-        content.append(datasource.getDescription());
-
-        content.append("\n\n[cols=2*,options=header]\n|===\n|Attribute\n|Value\n\n");
-        content.append("|Driver\n|");
-        content.append(datasource.getDriver());
-
-        content.append("\n\n|Url\n|");
-        content.append(datasource.getUrl());
-
-        content.append("\n|===\n\n");
-
-        content.append("== Tables\n\n");
-
-        return content.toString();
+        String content = "= " +
+                datasource.getName() +
+                "\nv0.5.0, 2020-03-24\n\n" +
+                datasource.getDescription() +
+                "\n\n[cols=2*,options=header]\n|===\n|Attribute\n|Value\n\n" +
+                "|Driver\n|" +
+                datasource.getDriver() +
+                "\n\n|Url\n|" +
+                datasource.getUrl() +
+                "\n|===\n\n" +
+                "== Tables\n\n";
+        return content;
     }
 
     @Override
     public String applyFormatToTable(Table table) {
-        StringBuilder content = new StringBuilder();
-        content.append("=== ");
-        content.append(table.getName());
-        content.append("\n\n");
-        content.append(table.getDocumentation());
-        content.append("\n\n==== Columns");
-        content.append("\n\n");
-        return content.toString();
+        String content = "=== " +
+                table.getName() +
+                "\n\n" +
+                table.getDocumentation() +
+                "\n\n==== Columns" +
+                "\n\n";
+        return content;
     }
 
     @Override
     public String applyFormatToColumn(Column column) {
-        StringBuilder content = new StringBuilder();
-        content.append("===== ");
-        content.append(column.getName());
-        content.append("\n\n");
-        content.append(column.getDocumentation());
-        content.append("\n\n====== Attributes");
-        content.append("\n\n");
-        return content.toString();
+        String content = "===== " +
+                column.getName() +
+                "\n\n" +
+                column.getDocumentation() +
+                "\n\n";
+        return content;
     }
 
     @Override
     public String applyFormatToIgnoredTable(IgnoredTable ignoredTable) {
-        StringBuilder content = new StringBuilder();
-        return content.toString();
+        return "";
     }
 
     @Override
     public String generateHTMLDocument(String asciidocContent) {
         Asciidoctor asciidoctor = Asciidoctor.Factory.create();
-        String httpContent = asciidoctor.convert(asciidocContent, new HashMap<String, Object>());
+        String httpContent = asciidoctor.convert(asciidocContent, new HashMap<>());
         httpContent = "<!DOCTYPE html><html lang=\"en\"><body>"+ httpContent +"</body></html>";
         return httpContent;
     }
