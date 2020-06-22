52f1ac6daff0bcf2db74f0426badd2874fbeb26a
Hildeberto Mendonca <me@hildeberto.com>
2020-06-22 08:32:24 -0400

    Fixed #17 Necessary change to deal with existing installations

--- src/main/resources/db/changelog/changes/00006-add-first-last-name-user.sql
+++ src/main/resources/db/changelog/changes/00006-add-first-last-name-user.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset htmfilho:6
-alter table users add first_name varchar(30) null;
-alter table users add last_name varchar(30) null;
+alter table users add first_name varchar(30) not null default '';
+alter table users add last_name varchar(30) not null default '';
 
 --rollback alter table users drop column first_name; alter table users drop column last_name;
