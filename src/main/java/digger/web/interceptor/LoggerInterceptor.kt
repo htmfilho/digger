770af59de6101dbfff4f4790344fb583dbcafdf0
Hildeberto Mendonca <me@hildeberto.com>
2022-10-03 22:24:28 -0400

    Completed migration of the web package to kotlin

--- src/main/java/digger/web/interceptor/LoggerInterceptor.kt
+++ src/main/java/digger/web/interceptor/LoggerInterceptor.kt
@@ -13,65 +13,51 @@
  * A full copy of the GNU General Public License is available at:
  * https://github.com/htmfilho/digger/blob/master/LICENSE
  */
+package digger.web.interceptor
 
-package digger.web.interceptor;
-
-import digger.model.User;
-import digger.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Enumeration;
+import digger.repository.UserRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Component
-public class LoggerInterceptor implements HandlerInterceptor {
-
-    private final static Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
-
-    private final UserRepository userRepository;
-
-    public LoggerInterceptor(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if(request.getUserPrincipal() == null) {
-            return true;
+class LoggerInterceptor(private val userRepository: UserRepository) : HandlerInterceptor {
+    @Throws(Exception::class)
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        if (request.userPrincipal == null) {
+            return true
         }
-
-        if("GET".equals(request.getMethod())) {
-            logger.info("{} navigates to {}", request.getUserPrincipal().getName() , request.getRequestURI() + getParameters(request));
+        if ("GET" == request.method) {
+            logger.info("{} navigates to {}", request.userPrincipal.name, request.requestURI + getParameters(request))
         }
-
-        HttpSession session = request.getSession(true);
+        val session = request.getSession(true)
         if (session.getAttribute("fullName") == null) {
-            User user = userRepository.findByUsername(request.getUserPrincipal().getName());
-            session.setAttribute("fullName", user.getFullName());
+            val user = userRepository.findByUsername(request.userPrincipal.name)
+            session.setAttribute("fullName", user.fullName)
         }
-
-        return true;
+        return true
     }
 
-    private String getParameters(HttpServletRequest request) {
-        Enumeration<?> e = request.getParameterNames();
+    private fun getParameters(request: HttpServletRequest): String {
+        val e: Enumeration<*>? = request.parameterNames
         if (e == null || !e.hasMoreElements()) {
-            return "";
+            return ""
         }
-
-        StringBuilder posted = new StringBuilder();
+        val posted = StringBuilder()
         while (e.hasMoreElements()) {
-            posted.append(posted.length() > 1 ? "&" : "?");
-            String curr = (String) e.nextElement();
-            posted.append(curr);
-            posted.append("=");
-            posted.append(request.getParameter(curr));
+            posted.append(if (posted.length > 1) "&" else "?")
+            val curr = e.nextElement() as String
+            posted.append(curr)
+            posted.append("=")
+            posted.append(request.getParameter(curr))
         }
-        return posted.toString();
+        return posted.toString()
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(LoggerInterceptor::class.java)
     }
 }
