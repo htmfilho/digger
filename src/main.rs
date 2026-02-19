d609085fb32ad260a5fbc602b698fa1b8f41209c
Hildeberto Mendonca <me@hildeberto.com>
2026-02-19 06:34:23 -0500

    Reading a database json file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Debug, Deserialize)]
+struct Database {
+    connection: Connection,
+    tables: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Connection {
+    url: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("geekswimmers.json")?;
+    let reader = BufReader::new(file);
+
+    let database: Database = serde_json::from_reader(reader)?;
+
+    for table in database.tables {
+        println!("select * from {} order by id", table);
+    }
+
+    Ok(())
 }
